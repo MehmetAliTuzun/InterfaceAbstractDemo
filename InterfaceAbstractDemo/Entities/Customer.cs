fb9ff5e4042e75fa1493b6c21a52050b6a33f21c
Mehmet Ali TÜZÜN <mehmetali2002tuzun@gmail.com>
2021-03-01 10:06:11 +0300

    Proje dosyası ekle.

---
+++ InterfaceAbstractDemo/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace InterfaceAbstractDemo.Entities
+{
+    public class Customer:IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NatId { get; set; }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            
+            //Enter a date of birth(day,month,year) in DateTime( , , )
+            
+            customerManager.Save(new Customer{DateOfBirth = new DateTime( , , ), FirstName = "Enter a name", LastName = "Enter a lastname", NatId= "Enter a nationality Id" });
+            Console.ReadLine();
+        }
+    }
+
+    
+
+}
